91f0ada1a80cec04284baf9e1e4b197e2187ba60
Tony Gaskell <thgaskell@gmail.com>
2013-12-06 00:41:36 -1000

    Timecode constructor throws errors if invalid input

--- test/timecode.js
+++ test/timecode.js
@@ -5,22 +5,49 @@ var chai = require('chai'),
     should = chai.should();
     Timecode = require('../timecode.js');
 
+chai.Assertion.includeStack = true
+
 describe('Timecode', function(){
-    it('should be a function', function(){
-        Timecode.should.be.a('function');
+    it('should validate strings as proper timecode format', function() {
+        Timecode.validate('00:00:00:00').should.be.true; // Non-drop frame
+        Timecode.validate('00:00:00;00').should.be.true; // Drop frame
+    });
+
+    describe('#constructor()', function() {
+        it('should accept either 1 string or 4 integers', function() {
+            new Timecode('00:00:00:00').should.exist;
+            new Timecode(0, 0, 0, 0).should.exist;
+        });
+        it('should allow user to override current Timecode configurations', function() {
+            new Timecode('00:00:00:00', 30).should.exist;
+            new Timecode(0, 0, 0, 0, false).should.exist;
+        });
+        it('should only accept a well-formed SMPTE timecode string', function() {
+            (function() {
+                new Timecode("invalid");
+            }).should.throw(RangeError);
+        });
+        it('should do type checking on input parameters', function() {
+            (function() {
+                new Timecode(false);
+            }).should.throw(RangeError);
+            (function() {
+                new Timecode(30, "00:00:00:00");
+            }).should.throw(RangeError);
+            (function() {
+                new Timecode("0", "0", "0", "0");
+            }).should.throw(RangeError);
+        });
     });
 });
 
 describe('Timecode instances', function() {
-    it('should not be an empty object', function(){
-        Object.keys(new Timecode()).length.should.be.above(0);
-    });
-
     describe('non-drop frame',function(){
         Timecode.config({dropframe: false});
         var timecode = new Timecode();
         it('should match the format hh:mm:ss:ff', function() {
-            timecode.toString().should.match(/\d{2,}:\d{2}:\d{2}:\d{2}/);
+            Timecode.validate(timecode.toString()).should.be.true;
+            timecode.toString().should.match(/^(\d){2}:(\d){2}:(\d){2}:(\d){2}$/);
         });
     });
 
@@ -28,7 +55,8 @@ describe('Timecode instances', function() {
         Timecode.config({dropframe: true});
         var timecode = new Timecode();
         it('should match the format hh:mm:ss;ff', function() {
-            timecode.toString().should.match(/\d{2,}:\d{2}:\d{2};\d{2}/);
+            Timecode.validate(timecode.toString()).should.be.true;
+            timecode.toString().should.match(/^(\d){2}:(\d){2}:(\d){2};(\d){2}$/);
         });
     });
 });
--- timecode.js
+++ timecode.js
@@ -1,9 +1,10 @@
 (function() {
     (function init() {
-        // Export the Timecode object for **Node.js**, with
-        // backwards-compatibility for the old `require()` API. If we're in
-        // the browser, add `Timecode` as a global object via a string identifier,
-        // for Closure Compiler "advanced" mode.
+        /* Export the Timecode object for **Node.js**, with
+         * backwards-compatibility for the old `require()` API. If we're in
+         * the browser, add `Timecode` as a global object via a string identifier,
+         * for Closure Compiler "advanced" mode.
+         */
         if (typeof exports !== 'undefined') {
             if (typeof module !== 'undefined' && module.exports) {
                 exports = module.exports = Timecode;
@@ -14,6 +15,7 @@
             this.Timecode = Timecode;
         }
 
+        //
         Timecode.prototype.toString = function() {
             var string = [this.hour, this.minute, this.second].map(function(value) {
                 return ('0' + value).slice(-2) })
@@ -27,28 +29,42 @@
     /* Initial values */
     Timecode.fps = 30;
     Timecode.dropframe = false;
-    Timecode.regex = /^(\d{2,}):(\d{2}):(\d{2})([:.;])(\d{2})$/;
+    var regex = /^(\d{2}):(\d{2}):(\d{2})([:;])(\d{2})$/,
 
     /* Supported framerates */
-    Timecode.domain = {
-        fps: [23.98, 24, 25, 29.97, 30]
-    };
+        domain = {
+            fps: [23.98, 24, 25, 29.97, 30]
+        };
 
+    /**
+     * validate() returns boolean representing whether the input
+     * is a well-fromed SMTPE timecode string
+     *
+     * @param <String> timecode
+     * @return <Boolean> validated Whether the string matches the timecode regex pattern 
+     */
     Timecode.validate = function(timecode) {
-        return Timecode.regex.test(timecode);
+        return regex.test(timecode);
     };
 
     Timecode.config = function(obj) {
-        if ( arguments.length > 0 && typeof obj === 'object' ){
-            if ( obj.hasOwnProperty('fps') && Timecode.domain.fps.indexOf(obj.fps) > -1 ) {
+        if (arguments.length > 0 && typeof obj === 'object') {
+            if (obj.hasOwnProperty('fps') &&
+                    domain.fps.indexOf(obj.fps) > -1) {
                 Timecode.fps = obj.fps;
             }
-            if ( obj.hasOwnProperty('dropframe') ){
+            if (obj.hasOwnProperty('dropframe')){
                 Timecode.dropframe = obj.dropframe;
             }
         }
     };
 
+    /**
+     * SMTPE Timecode
+     *
+     * @class Timecode 
+     * @constructor
+     */
     function Timecode() {
         this.hour = 0;
         this.minute = 0;
@@ -57,54 +73,68 @@
         this.fps = Timecode.fps;
         this.dropframe = Timecode.dropframe;
 
-        switch ( arguments.length ) {
-            case 3:
-                if ( typeof arguments[2] === 'boolean'
-                        && typeof arguments[0] === 'number' )
-                {
-                    this.dropframe = arguments[2];
+
+
+        (function inputValidation(arguments) {
+            var parsedValues;
+            switch (arguments.length) {
+
+                // "01:02:03:04", 30
+                case 2: {
+                    if (typeof arguments[0] == 'string' &&
+                            typeof arguments[1] == 'number' &&
+                            domain.fps.indexOf(arguments[1]) > -1) {
+                        this.fps = arguments[1];
+                    }
+                    else throw new RangeError();
                 }
-            case 2:
-                if ( typeof arguments[1] === 'number' 
-                        && Timecode.domain.fps.contains(arguments[1]) )
-                {
-                    this.fps = arguments[1];
-                }
-            case 1:
-                if ( typeof arguments[0] === 'string' 
-                     && Timecode.validate(arguments[0]) )
-                {
-                    var timecode = arguments[0].match(Timecode.regex);
-                    this.hour = timecode[1];
-                    this.minute = timecode[2];
-                    this.second = timecode[3];
-                    this.frame = timecode[5];
-                    this.dropframe = (timecode[4] !== ':');
-                }
-                else if ( typeof arguments[0] === 'number' ) {
-                    throw new RangeError("Currently unsupported.");
-                }
-                break;
-            case 6:
-                if ( typeof arguments[5] === 'boolean' ) {
-                    this.dropframe = arguments[5];
-                }
-            case 5:
-                if ( typeof arguments[4] === 'number' ) {
-                    this.fps = arguments[4];
-                }
-            case 4:
-                if ( Array.prototype.slice.call(arguments, 0,4).every(isNumber) ) {
-                    this.hour = arguments[0];
-                    this.minute = arguments[1];
-                    this.second = arguments[2];
-                    this.frame = arguments[3];
-                }
-                break;
-            case 0:
-            default:
-                break;
-        }
+
+                // "01:02:03:04" or "01:02:03;04"
+                case 1:
+                    if (typeof arguments[0] === 'string' &&
+                            Timecode.validate(arguments[0])) {
+                        parsedValues = arguments[0].split(/[:;]/);
+                        parsedValues.every(function(value) {
+                            return Number(value);
+                        });
+                        this.hour = parsedValues[0];
+                        this.minute = parsedValues[1];
+                        this.second = parsedValues[2];
+                        this.frame = parsedValues[3];
+                        this.dropframe = arguments[0].indexOf(";") > -1;
+                    }
+                    else throw new RangeError();
+                    break;
+
+                // 0, 1, 2, 3, 4, false
+                case 5: 
+                    if (typeof arguments[4] === 'boolean') {
+                        this.dropframe = arguments[4];
+                    }
+                    else throw new RangeError();
+
+                // 0, 1, 2, 3
+                case 4:
+                    var parameters = [].slice.call(arguments, 0, 4);
+                    if (parameters.every(function(parameter) {
+                            return typeof parameter === 'number';
+                    })) {
+                        parameters = parameters.map(function(parameter) {
+                            return Math.floor(parameter);
+                        });
+                        this.hour = parameters[0];
+                        this.minute = parameters[1];
+                        this.second = parameters[2];
+                        this.frame = parameters[3];
+                    }
+                    else throw new RangeError();
+                    break;
+                case 0: 
+                    break;
+                default:
+                    throw new RangeError();
+            }
+        })(arguments);
 
         function getFrame2997(frameNumber) {
             var times, hour, minute, second, frame, totalMinutes, frameNumber;
@@ -140,7 +170,4 @@
 
     return Timecode;
 
-    function isNumber(arg) {
-        return typeof arg === 'number'
-    };
 })();
